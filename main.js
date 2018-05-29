1d4768711cf0af8112044dfdbf55bac16ffdb08b
wangfuxuan <931788336@qq.com>
2018-05-29 10:00:35 +0800

    copy liu

--- main.js
+++ main.js
@@ -71,6 +71,8 @@ var ws2=fs.createWriteStream('output2.txt');
 ws2.write(new Buffer('使用stream写入二进制数据...、n','utf-8'));
 ws2.write(new Buffer('END',"utf-8"));
 ws2.end();
+var ws=fs.createWriteStream('copied.txt');
+rs.pipe(ws);
 // con(s); // Hello, Michael!
 // var arr=['sd','fd','dfad','fdsf','ad'];
 // for (const i of arr) {
